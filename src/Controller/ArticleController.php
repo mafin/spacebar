603601c9056f27abdaec39f9bd39c43bbecda4b5
Petr Enin <petr.enin@gmail.com>
2020-04-02 13:24:45 +0200

    add templates

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,10 +2,11 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-final class ArticleController
+final class ArticleController extends AbstractController
 {
     /**
      * @Route("/")
@@ -18,10 +19,21 @@ final class ArticleController
 
     /**
      * @Route("/news/{slug}")
+     * @param string $slug
      * @return Response
      */
     public function show($slug): Response
     {
-        return new Response('SHOW: ' . $slug);
+        $comments = [
+            'I ate a normal rock once. It did NOT taste like bacon!',
+            'Woohoo! I\'m going on an all-asteroid diet!',
+            'I like bacon too! Buy some from my site! bakinsomebacon.com',
+        ];
+
+
+        return $this->render('article/show.html.twig', [
+            'title' => ucwords(str_replace('-', ' ', $slug)),
+            'comments' => $comments,
+        ]);
     }
 }
